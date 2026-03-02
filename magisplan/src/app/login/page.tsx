912a84d7cad83100cf3cce3a56adcea4936677e0
Mady <youngmadeleineh@gmail.com>
2026-03-02 23:27:34 +0800

    added css to login/signup pages

--- magisplan/src/app/login/page.tsx
+++ magisplan/src/app/login/page.tsx
@@ -31,11 +31,43 @@ export default function LoginPage() {
   }
 
   return (
-    <form onSubmit={handleLogin}>
-      <input type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="Email" />
-      <input type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Password" />
-      <button type="submit">Login</button>
-      {error && <p>{error}</p>}
-    </form>
+    <div className="min-h-screen flex">
+
+      {/*app name + desc*/}
+      <div className="w-1/2 bg-[var(--main)] text-white flex flex-col justify-center p-12 rounded-2xl text-start">
+        <h1 className="text-5xl font-bold mb-4 ml-10">
+          Welcome to
+          MagisPlan!
+        </h1>
+        <p className="text-md mx-10">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
+      </div>
+
+      {/*log in essentials*/}
+      <div className="w-1/2 flex justify-center items-center text-center">
+        <form onSubmit={handleLogin} className="p-8 w-96 space-y-4">
+          <h2 className="text-2xl font-semibold text-center mb-4">
+            Login
+          </h2>
+
+          <input className="input-field" placeholder="Your email" type="email" value={email} onChange={e => setEmail(e.target.value)}/>
+          <input className="input-field" placeholder="Your password" type="password" value={password} onChange={e => setPassword(e.target.value)}/>
+
+          <p className="mt-8">
+            Don't have an account? Sign up here.
+          </p>
+          <button type="submit" className="btn-primary">Login</button>
+
+          {error && (
+            <p className="text-center text-sm text-red-500">
+              {error}
+            </p>
+          )}
+        </form>
+      </div>
+
+    </div>
   );
 }
--- magisplan/src/app/signup/page.tsx
+++ magisplan/src/app/signup/page.tsx
@@ -33,15 +33,48 @@ export default function SignupPage() {
   }
 
   return (
-    <form onSubmit={handleSignup}>
-      <input placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
-      <input placeholder="Password" type="password" value={password} onChange={e => setPassword(e.target.value)} />
-      <input placeholder="First Name" value={firstName} onChange={e => setFirstName(e.target.value)} />
-      <input placeholder="Middle Name" value={middleName} onChange={e => setMiddleName(e.target.value)} />
-      <input placeholder="Last Name" value={lastName} onChange={e => setLastName(e.target.value)} />
-      <input placeholder="Contact Number" value={contactNumber} onChange={e => setContactNumber(e.target.value)} />
-      <button type="submit">Sign Up</button>
-      {message && <p>{message}</p>}
-    </form>
-  )
+    <div className="min-h-screen flex">
+
+      {/*app name + desc*/}
+      <div className="w-1/2 bg-[var(--main)] text-white flex flex-col justify-center p-12 rounded-2xl text-start">
+        <h1 className="text-5xl font-bold mb-4 ml-10">
+          Welcome to
+          MagisPlan!
+        </h1>
+        <p className="text-md mx-10">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
+      </div>
+
+      {/*sign in essentials*/}
+      <div className="w-1/2 flex justify-center items-center text-center">
+        <form onSubmit={handleSignup} className="p-8 w-96 space-y-4">
+          <h2 className="text-2xl font-semibold text-center mb-4">
+            Sign Up
+          </h2>
+
+          <input className="input-field" placeholder="Your email" value={email} onChange={e => setEmail(e.target.value)}/>
+          <input className="input-field" placeholder="Your first name" value={firstName} onChange={e => setFirstName(e.target.value)} />
+          <input className="input-field" placeholder="Your middle name" value={middleName} onChange={e => setMiddleName(e.target.value)} />
+          <input className="input-field" placeholder="Your last name" value={lastName} onChange={e => setLastName(e.target.value)} />
+          <input className="input-field" placeholder="Your contact number" value={contactNumber} onChange={e => setContactNumber(e.target.value)} />
+          <input className="input-field" placeholder="Create password" type="password" value={password} onChange={e => setPassword(e.target.value)} />
+
+          <p className="mt-8">
+            Already have an account? Log in here.
+          </p>
+          <button type="submit" className="btn-primary">Sign Up</button>
+
+          {message && (
+            <p className="text-center text-sm text-red-500">
+              {message}
+            </p>
+          )}
+        </form>
+      </div>
+
+    </div>
+  );
+
 }
