b71a7d84e549b1d370e641d179a7fe558d18b98f
Mady <youngmadeleineh@gmail.com>
2026-04-10 01:37:40 +0800

    updated layout.tsx

--- magisplan/src/app/(app)/layout.tsx
+++ magisplan/src/app/(app)/layout.tsx
@@ -1,8 +1,6 @@
-// import type { Metadata } from "next";
-"use client";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
-import { useState } from "react";
+import SidebarLayout from "@/components/SidebarLayout";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,57 +12,13 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  const [expanded, setExpanded] = useState(true);
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={expanded ? "sb-expanded" : ""}>
-        <aside>
-          <nav>
-            <ul>
-              <li>
-                <p className="text-2xl font-bold titleEx text-white py-5">MagisPlan</p>
-              </li>
-              <li>
-                <a href="#">
-                  <img src="/home.svg" alt="Icon" width={30} />
-                  <span>Home</span>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <img src="/chat.svg" alt="Icon" width={30} />
-                  <span>Chat</span>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <img src="/profile.svg" alt="Icon" width={30} />
-                  <span>Profile</span>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <img src="/project.svg" alt="Icon" width={30} />
-                  <span>My Projects</span>
-                </a>
-              </li>
-              <li>
-                <a onClick={() => setExpanded(!expanded)}>
-                    <img src="/arrow.svg" alt="Icon" width={30} className="collapsed" />
-                  <span>Collapse</span>
-                </a>
-              </li>
-            </ul>
-          </nav>
-        </aside>
-        <main  className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <SidebarLayout>
           {children}
-        </main>
+        </SidebarLayout>
       </body>
     </html>
   );
