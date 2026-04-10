2593e543fc90f1e02d29778a300c6460c73bc104
Mady <youngmadeleineh@gmail.com>
2026-04-10 11:47:57 +0800

    fix merge conflicts

--- magisplan/src/app/(app)/layout.tsx
+++ magisplan/src/app/(app)/layout.tsx
@@ -1,7 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
 import SidebarLayout from "@/components/SidebarLayout";
-import { useState } from "react";
 import LogoutButton from "@/components/LogoutButton";
 
 const geistSans = Geist({
